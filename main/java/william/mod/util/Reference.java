d94554e2911825f6f51b58ac336239206d351a2c
NightmistMods <55203961+NightmistMods@users.noreply.github.com>
2019-09-11 18:49:37 +0200

    Add files via upload

---
+++ main/java/william/mod/util/Reference.java
@@ -0,0 +1,10 @@
+package william.mod.util;
+
+public class Reference
+{
+	public static final String MODID = "wmii";
+	public static final String NAME = "William Mod II";
+	public static final String VERSION = "1.0";
+	public static final String CLIENT = "william.mod.proxy.ClientProxy";
+	public static final String COMMON = "william.mod.proxy.CommonProxy";
+}
